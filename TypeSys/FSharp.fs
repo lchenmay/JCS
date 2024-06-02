d9725eae51ebd3a04653f4b433abb7eadb545445
RR <RR@MAIN>
2024-06-02 15:37:24 -0400

    s

--- TypeSys/FSharp.fs
+++ TypeSys/FSharp.fs
@@ -3,6 +3,7 @@
 open System
 open System.Collections.Generic
 open System.Text
+open System.Text.RegularExpressions
 
 open Util.Text
 open Util.FileSys
@@ -33,33 +34,95 @@ let symbols =
         "="
         ":" |]
 
-type Token = 
+type Token =
+| Newline of int // int = indent
+| Comment of string
 | Reserved of string
 | Identifier of string
 | Symbol of string
 | Quoted of string
 | Literal of string
-| Undefined
+| Undefined of string
 
-type Line = {
-src: string
-tokens: Token[]
-indent: int }
+type LexState = 
+| Normal
+| Quoted
+| Comment
 
-type FSharpCompiler = {
-output: string -> unit }
 
-let lex txt = 
+let regex_options = RegexOptions.Multiline ||| RegexOptions.ExplicitCapture ||| RegexOptions.Compiled
 
-    [| |]
+let string__regex s = new Regex(s,regex_options)
 
-let txt__line (txt:string) = 
+let r0 = 
+    // (* """ " // 
+    @"(\(\*|0x220x220x22|0x22|//)" 
+    |> string__regex
 
-    let trim = txt.TrimStart()
 
-    {   src = txt
-        tokens = lex txt
-        indent = txt.Length - trim.Length }
+let lex (src:string) = 
+
+    let index = ref 0
+
+    let mask = Array.zeroCreate src.Length
+
+    let tokens = new List<Token>()
+
+    let maskNext 
+        (index:int ref)
+        (src:string) 
+        (pattern:string) 
+        (m:System.Text.RegularExpressions.Match) 
+        wrapper = 
+
+        let e = 
+            let starting = m.Index + m.Value.Length
+            let ending = src.IndexOf(pattern,starting)
+            if ending >= starting then
+                ending + pattern.Length
+            else
+                src.Length - 1
+
+        index.Value <- e
+
+        src.Substring(m.Index,e - m.Index) 
+        |> wrapper
+        |> tokens.Add
+
+    while index.Value < src.Length do
+        let m = r0.Match(src,index.Value)
+        if m.Success then
+            if m.Index > index.Value then
+                src.Substring(index.Value, m.Index - index.Value) 
+                |> Token.Undefined
+                |> tokens.Add
+            match m.Value with
+            | "(*" -> maskNext index src "*)" m Token.Comment
+            | "\"\"\"" -> maskNext index src "\"\"\"" m Token.Quoted
+            | "\"" -> maskNext index src "\"" m Token.Quoted
+            | "//" -> maskNext index src crlf m Token.Comment
+            | _ -> ()
+        else
+            src.Substring(index.Value, src.Length - index.Value) 
+            |> Token.Undefined
+            |> tokens.Add
+            index.Value <- src.Length
+
+    let items = tokens.ToArray()
+
+    tokens.Clear()
+
+    items
+    |> Array.iter(fun t -> 
+        match t with
+        | Token.Undefined s -> 
+            s.Split crlfcrlf
+            |> Array.map Token.Undefined
+            |> tokens.AddRange
+        | _ -> tokens.Add t)
+
+    tokens.ToArray()
+
 
 let removeComment = Array.map(fun (i:string) -> 
     let index = i.IndexOf "//"
@@ -68,16 +131,19 @@ let removeComment = Array.map(fun (i:string) ->
     else
         i)
 
+type FSharpCompiler = {
+output: string -> unit }
+
 let go output file = 
     
     let compiler = {
         output = output }
 
     let lines = 
-        let error,lines = file |> try_read_lines
-        lines
-        |> removeComment
-        |> Array.map txt__line
+        file 
+        |> try_read_string
+        |> snd
+        |> lex
 
     ()
 
