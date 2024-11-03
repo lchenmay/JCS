96add1cee66b3450f706526b6500c0ed0ab739ca
R <siduochen@hotmail.com>
2024-11-02 22:35:42 -0400

    s

--- vscode/src/main.ts
+++ vscode/src/main.ts
@@ -17,5 +17,5 @@ glib.notify.init()
 //if (beforeApp) {beforeApp()}
 const app = glib.vue.createApp(App)
 app.use(runtime.router).mount('#app')
-glib.route.router.push('/index')
+glib.route.router.push('/')
 
